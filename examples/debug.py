40044068643b7eb8503834464f4e03bf8e944fb6
cvcvka5 <94840634+cvcvka5@users.noreply.github.com>
2025-04-06 01:26:36 +0300

    Update debug.py

--- examples/debug.py
+++ examples/debug.py
@@ -4,5 +4,5 @@ from pygpt import PyGPT
 gpt = PyGPT(fjs_base=open("./util/frame.txt", "r"), js_base_holder="//##PYGPT##", debug=True, finitprompt=None)
 
 while True:
-    print("out:", gpt.ask(input("sor")))
+    print("out:", gpt.ask(input("ask")))
     print()
